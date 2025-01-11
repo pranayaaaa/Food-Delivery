147d039d6faaedab7e6321723ff539d9e11365ed
Pranaya <pranayapounikar24@gmail.com>
2025-01-11 13:01:18 +0530

    update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder=async(req,res)=>{
 
-    const frontend_url="http://localhost:5173"
+    const frontend_url="https://food-delivery-1-yp97.onrender.com/"
 
     try{
         const newOrder=new orderModel({
